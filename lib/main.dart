4eee1c6490b00e105cde12b5b2ffe47d52dd64d9
Rafael Escobar Gutierrez <rescobar@bit.lat>
2024-05-23 11:28:38 -0600

    Agrega multiprovider a todo el arbol de widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:maybe_app/config/themes/app_theme.dart';
+import 'package:maybe_app/domain/entities/providers/chat_provider.dart';
 import 'package:maybe_app/presentation/screens/chat/chat_screen.dart';
+import 'package:provider/provider.dart';
 
 void main(){
   runApp(MyApp());
@@ -11,10 +13,15 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: AppTheme(selectedColor: 3).theme(),
-      home: const ChatScreen()
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => ChatProvider())
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: AppTheme(selectedColor: 3).theme(),
+        home: const ChatScreen()
+      ),
     );
   }
 
