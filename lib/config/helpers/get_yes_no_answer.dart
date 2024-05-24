559de436f2f633a12a325e910852387c78d0f84a
Rafael Escobar Gutierrez <rescobar@bit.lat>
2024-05-24 09:31:54 -0600

    Agrega respuesta del destinatario en base a respuesta del api

---
+++ lib/config/helpers/get_yes_no_answer.dart
@@ -0,0 +1,19 @@
+import 'package:dio/dio.dart';
+import 'package:maybe_app/domain/entities/message.dart';
+import 'package:maybe_app/infrastructure/models/yes_no_model.dart';
+
+mixin class GetYesNoAnswer {
+  final _dio = Dio();
+
+  Future<Message> getAnswer() async {
+    final response = await _dio.get('https://yesno.wtf/api');
+    final mappingResponse = YesNoModel.fromJsonMap(response.data);
+
+    return Message(
+      message: mappingResponse.answer,
+      fromWho: FromWho.destination,
+      imgUrl: mappingResponse.image
+    );
+
+  }
+}
--- lib/domain/entities/providers/chat_provider.dart
+++ lib/domain/entities/providers/chat_provider.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:maybe_app/config/helpers/get_yes_no_answer.dart';
 import 'package:maybe_app/domain/entities/message.dart';
 
 class ChatProvider extends ChangeNotifier {
@@ -7,15 +8,24 @@ class ChatProvider extends ChangeNotifier {
   List<Message> messages = [
     Message(message: 'Hola!!', fromWho: FromWho.me),
     Message(message: 'Como estas??', fromWho: FromWho.me),
-    Message(message: 'Aqui andamos', fromWho: FromWho.me),
   ];
 
   Future<void> sendMessage( String message ) async {
     if (message.trim().isEmpty) return;
     final newMessage = Message(message: message, fromWho: FromWho.me);
     messages.add(newMessage);
+
+    if(message.endsWith('?')) {
+      await herReply();
+    }
+
     notifyListeners();
     await moveScrollToBottom();
+  }
+
+  Future<void> herReply() async {
+    final herMessage = await GetYesNoAnswer().getAnswer();
+    messages.add(herMessage);
   }
 
   Future<void> moveScrollToBottom() async {
